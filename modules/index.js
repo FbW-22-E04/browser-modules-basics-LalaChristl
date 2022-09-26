5323893b9e558e853c83eced3a891213d339c1d9
LalaChristl <lesleyannchristl@gmail.com>
2022-09-26 11:45:32 +0200

    added solution

---
+++ modules/index.js
@@ -0,0 +1,17 @@
+import {hoverHeader} from "./hover.js";
+import {scrollWindow} from "./scroll.js";
+import {clickImages} from "./click.js"
+
+
+
+
+
+
+
+
+
+
+
+hoverHeader()
+scrollWindow()
+clickImages()
